d9fe116f2ac0632f38f709f67a66c0d04b4dcd68
GOPIKAPANDI <gopikapandi.r2021csea@sece.ac.in>
2023-03-12 14:38:03 +1030

    Nav-bar-Search-window-Add-task completed

---
+++ index.js
@@ -0,0 +1,42 @@
+// creating an object -->state
+// var state={
+// creating an array taskList
+// taskList: [
+//     {
+//         imageUrl: "",
+//         taskTitle: "",
+//         taskType: "",
+//         taskDescription: "",
+//     },
+//     {
+//         imageUrl: "",
+//         taskTitle: "",
+//         taskType: "",
+//         taskDescription: "",
+//     },
+//     {
+//         imageUrl: "",
+//         taskTitle: "",
+//         taskType: "",
+//         taskDescription: "",
+//     },
+//     {
+//         imageUrl: "",
+//         taskTitle: "",
+//         taskType: "",
+//         taskDescription: "",
+//     },
+//     {
+//         imageUrl: "",
+//         taskTitle: "",
+//         taskType: "",
+//         taskDescription: "",
+//     },
+
+// ]
+// };
+const state={
+    taskList:[],
+};
+const taskContents = document.querySelector(".task__contents")
+const taskModal = document.querySelector(".task__modal__body")
