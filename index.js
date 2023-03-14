8e238d592d5fc45cd88f353617ba2cf6e1aae1f8
GOPIKAPANDI <gopikapandi.r2021csea@sece.ac.in>
2023-03-14 21:19:25 +1030

    handlesubmit half done

--- index.js
+++ index.js
@@ -34,11 +34,14 @@
 //     },
 // ]
 // };
+// This array taskList is a backup storage
 const state={
     taskList:[],
 };
 const taskContents = document.querySelector(".task__contents")
 const taskModal = document.querySelector(".task__modal__body")
+
+// getting some js element to html use "$".....
 const htmlTaskContent = ({ id, title , description, type, url })=>`
     <div class='col-md-6 col-lg-4 mt-3' id=${id}>
         <div class="card shadow-sm task__card " >
@@ -89,4 +92,48 @@ const htmlModalContent = ({ id, title , description, type, url })=>{
     </div>
     `;
 };
+// we convert json to str (i.e., for local storage)
+const updateLocalStorage = () => {
+    localStorage.setItem(
+     "task",  
+     JSON.stringify({
+        tasks: state.taskList,
+     })
+    //  here we key:value as ---> task : tasks
+    );
+};
+// Load Initial Data
+// we convert string to json (i.e., for getting cards on the screen)
+const LoadInitialData = () => {
+const localStorageCopy = JSON.parse(localStorage.task);
 
+if(localStorageCopy) state.taskList = localStorageCopy.task;
+
+state.taskList.map((cardDate)=>{
+    taskContents.innerAdjacentHTML("beforeend", htmlTaskContent(cardDate));//innerAdjacentHTML => to get the cards side by side
+});
+};
+
+// when we edit or update we need to save
+// to give submit(save changes) to the particular card we need to denote the card with ID
+// getting some html stuffs to js that is while clicking submit in html output(i.e., card)
+// we have to store those changes in js.....
+
+const handleSubmit=(event)=>{   
+    const id= `${Date.now()}`;
+    const input={
+        url:document.getElementById("imageUrl").value,  //url : https.... now the url will be user given input url
+        title:document.getElementById("taskTitle").value,
+        tags:document.getElementById("tags").value,
+        taskDescription:document.getElementById("taskDescription").value,  
+    };        // to display both updated and non updated cards in UI
+    if(input.title==""||input.tags==""||input.taskDescription==""){
+        return alert("Please fill all the necessary fields ...");
+    }
+    taskContents.innerAdjacentHTML("beforeend",htmlTaskContent({...input, id}));
+    state.taskList.push({...input, id});       //store this updated data in array
+    updateLocalStorage();
+};
+
+// while we update and click "save changes" button all the datas should be saved...so we have to call the method
+// "handleSubmit" in html ...to save the changes.
